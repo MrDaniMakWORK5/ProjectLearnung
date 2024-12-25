bcab83c7e2493b9e35f5f22d14b72f0e8b0ae66e
unknown <mrdanimak@gmail.com>
2024-12-25 09:44:49 +0300

    styled with ClangFormat

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main(){
+    //Program about Welcoming poeple
     //Adding string valuable name
-    string name;
+    string name, surname;
     //Entering name
-    cin >> name;
+    cin >> name >> surname;
     //Console output greting
-    cout << "Hello " << name << " how are you doing?";
+    cout << "Hello " << name << " " << surname << " how are you doing?";
     return 0;
 }
